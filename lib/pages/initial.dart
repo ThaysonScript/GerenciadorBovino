409c4620b372f83a9f74ae1a0879ea20a8b0c2da
ThaysonScript <thaysonguedes01@gmail.com>
2024-09-27 12:26:48 -0300

    [update] - pages/initial

--- lib/pages/initial.dart
+++ lib/pages/initial.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gerenciador_bovino/pages/auth/cadastro.dart';
+import 'package:gerenciador_bovino/pages/auth/login.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Initial extends StatelessWidget {
   const Initial({super.key});
@@ -6,45 +9,90 @@ class Initial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Image.asset(
-                "assets/images/bovino02.jpg",
-                height: 500, // Altura da imagem
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-
-          Positioned(
-            top: 455,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
+        body: Stack(
+      children: [
+        Image.asset("assets/images/bovino02.jpg"),
+        Padding(
+          padding: const EdgeInsets.only(top: 370),
+          child: Container(
+              width: double.infinity,
+              height: double.infinity,
               decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-              ),
-
-              child: const Column(
+                  color: Color.fromRGBO(235, 231, 217, 1),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40))),
+              child: Column(
                 children: [
-                  Text("Gerenciador Bovino",
-                    style: TextStyle(fontSize: 40),
+                  const SizedBox(
+                    height: 20,
                   ),
+                  Text("Gerenciador Bovino",
+                      style: GoogleFonts.anton(
+                          fontSize: 40, fontWeight: FontWeight.normal)),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                      right: 20,
+                    ),
+                    child: Text(
+                      '''
+                        Lorem ipsum dolor sit amet. Est voluptas enim ut inventore voluptatem  sit magnam harum.
+                        Non minima laboriosam ad pariatur omnis aut maiores  quaerat non dolores dolores.
+                        Qui quos sunt et consequatur inventore id  suscipit delectus et dolores maiores.
+                        ''',
+                      textAlign: TextAlign.justify,
+                      style: GoogleFonts.anton(fontSize: 16),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => {
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Login()))
+                    },
+                    child: Container(
+                      width: 300,
+                      height: 60,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: Colors.amber,
+                      ),
+                      child: Center(
+                        child: Text("Login", 
+                          style: GoogleFonts.anton(fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30,),
+                  GestureDetector(
+                    onTap: () => {
+                      Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const Cadastro()))
+                    },
+                    child: Container(
+                      width: 300,
+                      height: 60,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: Colors.amber,
+                      ),
+
+                      child: Center(
+                        child: Text("Cadastro",
+                          style: GoogleFonts.anton(fontSize: 35),
+                        ),
+                      ),
+                    ),
+                  )
                 ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+              )),
+        ),
+      ],
+    ));
   }
 }
